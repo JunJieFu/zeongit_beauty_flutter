7399ca134dbffc986f4abd0cdf71cbbeed73b66c
Zeongit <245142637@qq.com>
2020-06-29 16:59:07 +0800

    .

--- lib/widget/fragment/list_waterfall.widget.dart
+++ lib/widget/fragment/list_waterfall.widget.dart
@@ -7,6 +7,7 @@ import 'package:zeongitbeautyflutter/assets/entity/picture_entity.dart';
 import 'package:zeongitbeautyflutter/assets/style/index.style.dart';
 import 'package:zeongitbeautyflutter/assets/util/image.util.dart';
 import 'package:zeongitbeautyflutter/pages/detail.page.dart';
+import 'package:zeongitbeautyflutter/widget/fragment/ink_clip.widget.dart';
 
 class ListWaterFallWidget extends StatefulWidget {
   ListWaterFallWidget({Key key, this.page, this.loading, this.pageable})
@@ -30,19 +31,39 @@ class _ListWaterFallWidgetState extends State<ListWaterFallWidget> {
         //cacheExtent: 0.0,
         padding: EdgeInsets.all(listGap),
         gridDelegate: SliverWaterfallFlowDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2, crossAxisSpacing: listGap, mainAxisSpacing:listGap),
+            crossAxisCount: 2,
+            crossAxisSpacing: listGap,
+            mainAxisSpacing: listGap),
         children: widget.page?.content?.map((PictureEntity picture) {
               return Ink(
-                  child: InkWell(
-                    child: Image.network(
-                        ImageUtil.picture(picture.url, type: ImageType.specifiedWidth),
-                        fit: BoxFit.cover),
+                child: InkWell(
+                    child: Container(
+                        child: Ink.image(
+                            image: NetworkImage(ImageUtil.picture(
+                                "81754094_p0.jpg",
+                                type: ImageType.specifiedWidth)),
+                            fit: BoxFit.cover)),
                     onTap: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (_) {
-                        return DetailPage(id: picture.id);
-                      }));
-                    },
-                  ));;
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (_) => DetailPage(id: picture.id)));
+                    }),
+              );
+
+//                  Ink(
+//                      child: InkWell(
+//                child: Container(
+//                    child: Image(
+//                        image: NetworkImage(ImageUtil.picture("81754094_p0.jpg",
+//                            type: ImageType.specifiedWidth)),
+//                        fit: BoxFit.cover)),
+//                onTap: () {
+//                  Navigator.push(context, MaterialPageRoute(builder: (_) {
+//                    return DetailPage(id: picture.id);
+//                  }));
+//                },
+//              ));
             })?.toList() ??
             []);
   }
