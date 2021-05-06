df508c2cd0f432afd7dbbe54ae4a36a2582316d6
Zeongit <245142637@qq.com>
2021-05-06 17:14:55 +0800

    更多

--- lib/widgets/btn/more_icon_btn.widget.dart
+++ lib/widgets/btn/more_icon_btn.widget.dart
@@ -4,12 +4,10 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:provider/provider.dart';
 import 'package:zeongitbeautyflutter/assets/constants/enum.constant.dart';
 import 'package:zeongitbeautyflutter/assets/entity/picture_entity.dart';
-import 'package:zeongitbeautyflutter/assets/services/index.dart';
 import 'package:zeongitbeautyflutter/pages/picture/edit.page.dart';
 import 'package:zeongitbeautyflutter/plugins/styles/index.style.dart';
 import 'package:zeongitbeautyflutter/plugins/styles/mdi_icons.style.dart';
 import 'package:zeongitbeautyflutter/plugins/utils/build.util.dart';
-import 'package:zeongitbeautyflutter/plugins/utils/result.util.dart';
 import 'package:zeongitbeautyflutter/plugins/widgets/popup_container.widget.dart';
 import 'package:zeongitbeautyflutter/provider/user.provider.dart';
 import 'package:zeongitbeautyflutter/widgets/black_hole_dialog.widget.dart';
@@ -26,7 +24,7 @@ class MoreIconBtn extends StatelessWidget {
 
   final Function callback;
 
-  _remove(BuildContext context, int id) {
+  _remove(BuildContext context) {
     showDialog(
         context: context,
         builder: (ctx) {
@@ -63,38 +61,106 @@ class MoreIconBtn extends StatelessWidget {
         });
   }
 
+  _hide(BuildContext context) {
+    final privacy = picture.privacy == PrivacyState.PRIVATE.index;
+    showDialog(
+        context: context,
+        builder: (ctx) {
+          return AlertDialog(
+            title: Text("提示"),
+            content: Text("您确定${privacy ? "公开" : "隐藏"}该图片吗？"),
+            actions: <Widget>[
+              TextButton(
+                  style: TextButton.styleFrom(
+                    primary: StyleConfig.warningColor,
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop(this);
+                  },
+                  child: Text("取消")),
+              TextButton(
+                  onPressed: () async {
+                    Navigator.of(context).pop(this);
+                    Fluttertoast.showToast(
+                        msg: "尚未开发",
+                        gravity: ToastGravity.BOTTOM,
+                        backgroundColor: StyleConfig.successColor);
+                  },
+                  child: Text("确定"))
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     var userState = Provider.of<UserState>(context, listen: false);
     final privacy = picture.privacy == PrivacyState.PRIVATE.index;
-    var selfWidgetList = [
-      Divider(height: 1),
-      ListTile(
-        title: buildListTileTitle("编辑", leftIcon: MdiIcons.image_edit_outline),
-        onTap: () {
-          Navigator.push(context, MaterialPageRoute(builder: (_) {
-            return EditPage(picture, callback: (picture) {});
-          }));
-        },
-      ),
-      Divider(height: 1),
-      ListTile(
-        title: buildListTileTitle(privacy ? "公开" : "隐藏",
-            leftIcon:
-                privacy ? MdiIcons.eye_outline : MdiIcons.eye_off_outline),
-        onTap: () {},
-      ),
-      Divider(height: 1),
-      ListTile(
-        title: buildListTileTitle("删除",
-            leftIcon:
-                privacy ? MdiIcons.eye_off_outline : MdiIcons.delete_outline),
-        onTap: () {
-          Navigator.of(context).pop(this);
-          _remove(context, picture.id);
-        },
-      )
-    ];
+    var widgetList = userState.info.id == picture.user.id
+        ? [
+            Divider(height: 1),
+            ListTile(
+              title: buildListTileTitle("编辑",
+                  leftIcon: MdiIcons.image_edit_outline),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (_) {
+                  return EditPage(picture, callback: (picture) {});
+                }));
+              },
+            ),
+            Divider(height: 1),
+            ListTile(
+              title: buildListTileTitle(privacy ? "公开" : "隐藏",
+                  leftIcon: privacy
+                      ? MdiIcons.eye_outline
+                      : MdiIcons.eye_off_outline),
+              onTap: () {
+                Navigator.of(context).pop(this);
+                _hide(context);
+              },
+            ),
+            Divider(height: 1),
+            ListTile(
+              title: buildListTileTitle("删除",
+                  leftIcon: privacy
+                      ? MdiIcons.eye_off_outline
+                      : MdiIcons.delete_outline),
+              onTap: () {
+                Navigator.of(context).pop(this);
+                _remove(context);
+              },
+            )
+          ]
+        : [
+            ListTile(
+                title: buildListTileTitle("屏蔽",
+                    leftIcon: MdiIcons.shield_off_outline),
+                onTap: () {
+                  Navigator.of(context).pop(this);
+                  showDialog(
+                      context: context,
+                      builder: (ctx) {
+                        return BlackHoleDialog(
+                          id: picture.id,
+                        );
+                      });
+                }),
+            Divider(height: 1),
+            ListTile(
+              title: buildListTileTitle("举报",
+                  leftIcon: MdiIcons.alert_octagon_outline),
+              onTap: () {
+                Navigator.of(context).pop(this);
+                showDialog(
+                    context: context,
+                    builder: (ctx) {
+                      return ComplaintDialog(
+                        picture: picture,
+                      );
+                    });
+              },
+            )
+          ];
     return IconButton(
       key: _btnKey,
       icon: Icon(Icons.more_vert),
@@ -110,39 +176,7 @@ class MoreIconBtn extends StatelessWidget {
                     width: 150,
                     child: Column(
                       mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        ListTile(
-                            title: buildListTileTitle("屏蔽",
-                                leftIcon: MdiIcons.shield_off_outline),
-                            onTap: () {
-                              Navigator.of(context).pop(this);
-                              showDialog(
-                                  context: context,
-                                  builder: (ctx) {
-                                    return BlackHoleDialog(
-                                      id: picture.id,
-                                    );
-                                  });
-                            }),
-                        Divider(height: 1),
-                        ListTile(
-                          title: buildListTileTitle("屏蔽",
-                              leftIcon: MdiIcons.alert_octagon_outline),
-                          onTap: () {
-                            Navigator.of(context).pop(this);
-                            showDialog(
-                                context: context,
-                                builder: (ctx) {
-                                  return ComplaintDialog(
-                                    picture: picture,
-                                  );
-                                });
-                          },
-                        ),
-                        ...(userState.info.id == picture.user.id
-                            ? selfWidgetList
-                            : [])
-                      ],
+                      children: widgetList,
                     ),
                   ),
                 ),
