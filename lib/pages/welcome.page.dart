9a3fc8a96b836f370c8cdf426f97e1520092af43
Zeongit <245142637@qq.com>
2021-05-06 17:57:43 +0800

    修改图片

--- lib/pages/welcome.page.dart
+++ lib/pages/welcome.page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:zeongitbeautyflutter/pages/home/tab.page.dart';
+import 'package:zeongitbeautyflutter/plugins/styles/index.style.dart';
 import 'package:zeongitbeautyflutter/provider/fragment.provider.dart';
 
 class WelcomePage extends StatelessWidget {
@@ -10,16 +11,39 @@ class WelcomePage extends StatelessWidget {
     return Scaffold(
         body: ConstrainedBox(
             constraints: BoxConstraints.expand(),
-            child: GestureDetector(
-                child: Image(
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                Image(
                     image: AssetImage('assets/images/welcome.png'),
                     fit: BoxFit.cover),
-                onTap: () async {
-                  fragmentState.updateHadInit();
-                  Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (_) {
-                    return TabPage();
-                  }));
-                })));
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(bottom: StyleConfig.gap * 10),
+                      child: SizedBox(
+                        width: 120,
+                        height: 48,
+                        child: OutlinedButton(
+                          style: OutlinedButton.styleFrom(
+                              textStyle: TextStyle(fontSize: 18,fontWeight: FontWeight.w300),
+                              primary: Colors.white,
+                              side: BorderSide(color: Colors.white,width: 1.5)),
+                          onPressed: () {
+                            fragmentState.updateHadInit();
+                            Navigator.pushReplacement(context,
+                                MaterialPageRoute(builder: (_) {
+                                  return TabPage();
+                                }));
+                          },
+                          child: Text("进入首页"),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            )));
   }
 }
